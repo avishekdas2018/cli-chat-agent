5e6277667a38428a07965c9b3853cbef4834d6fe
avishekdas2018 <dasavishek2013@gmail.com>
2026-06-30 19:42:29 +0530

    Add express dependency and enhance chat interface functionality
    - Added express as a dependency in package.json
    - Updated tsconfig.json to include DOM and ES2023 in lib
    - Modified index.html to improve chat container styling
    - Implemented message generation and handling in script.js

--- client/script.js
+++ client/script.js
@@ -1,1 +1,42 @@
+const input = document.querySelector("#input");
+const chatContainer = document.querySelector("#chat-container");
+const sendBtn = document.querySelector("#send");
+console.log(input);
 
+input.addEventListener("keyup", handleSubmitMessage);
+sendBtn.addEventListener("click", handleClick);
+
+function generate(text) {
+  /**
+   * 1. Append message to UI
+   * 2. Send it to the LLM
+   * 3. Append response to UI
+   */
+
+  const msg = document.createElement("div");
+  msg.className = `my-6 bg-[#2B2B2B] p-3 rounded-full ml-auto max-w-fit`;
+  msg.textContent = text;
+  chatContainer.appendChild(msg);
+  input.value = "";
+}
+
+function handleClick() {
+  const text = input.value.trim();
+  if (!text) {
+    return;
+  }
+
+  generate(text);
+}
+
+function handleSubmitMessage(e) {
+  if (e.key === "Enter") {
+    const text = input.value.trim();
+
+    if (!text) {
+      return;
+    }
+    generate(text);
+    console.log(text);
+  }
+}
